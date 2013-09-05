74cf647215db238270f26649fd8cca56397f47d9
Jonatas Oliveira <jonatas.oliveira@gmail.com>
2013-09-05 18:45:29 -0300

    Implementing get() on GeneratedsSuper and fixing the choosing of CPFCNPJTomador type.

--- pysped_nfse/processador_sp.py
+++ pysped_nfse/processador_sp.py
@@ -356,6 +356,12 @@ class ProcessadorNFSeSP(ProcessadorBase):
                 tipo_inscricao=rps.get('TipoInscricaoTomador'),
                 cpf_cnpj=rps.get('CPFCNPJTomador'),
                 )
+
+            if len(rps.get('CPFCNPJTomador', '')) > 11:
+                cpf_cnpj_tomador = tpCPFCNPJ(CNPJ=rps.get('CPFCNPJTomador'))
+            else:
+                cpf_cnpj_tomador = tpCPFCNPJ(CPF=rps.get('CPFCNPJTomador'))
+
             rps_obj = FixedRPS(
                 Assinatura=assinatura,
                 ChaveRPS=tpChaveRPS(
@@ -377,7 +383,7 @@ class ProcessadorNFSeSP(ProcessadorBase):
                 CodigoServico=rps.get('CodigoServico'),
                 AliquotaServicos=rps.get('AliquotaServicos'),
                 ISSRetido=rps.get('ISSRetido'),
-                CPFCNPJTomador=tpCPFCNPJ(CNPJ=rps.get('CPFCNPJTomador')),
+                CPFCNPJTomador=cpf_cnpj_tomador,
                 InscricaoMunicipalTomador=inscr_mun_tomador,
                 InscricaoEstadualTomador=inscr_est_tomador,
                 RazaoSocialTomador=self._remove_accents(
